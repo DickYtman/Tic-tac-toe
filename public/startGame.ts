c4a83a214b095df620b91f78bbfa70887fb23138
hadeer57 <hadeer57@hotmail.com>
2022-08-02 22:49:58 +0300

    Project

---
+++ public/startGame.ts
@@ -0,0 +1,12 @@
+
+function profile(){
+    const selectedSymbol = document.querySelectorAll(".imgPlayer, .idImg");
+    selectedSymbol.forEach(imgPlayer =>{
+imgPlayer.addEventListener("click", e=>{
+    let target= e.target
+}
+)
+    })
+}
+
+profile();
